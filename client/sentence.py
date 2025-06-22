8089e746b2bc8e5c5c776622be5159888f6138ea
Aranza Rodriguez <rodriguez.aranza.9801@gmail.com>
2025-06-22 17:31:28 -0400

    Adding sentence and sentiment Analysis files

---
+++ client/sentence.py
@@ -0,0 +1,24 @@
+class Sentence:
+    #define the sentence object
+  
+    def __init__(self, sentence="", positive=0.0, negative=0.0, neutral=0.0, startTime=0.0, endTime=0.0):
+        self.sentence = sentence
+        self.positive = positive
+        self.negative = negative 
+        self.neutral = neutral
+        self.startTime = startTime
+        self.endTIme = endTime
+
+    def summary(self):
+        return {
+            "sentence": self.sentence,
+            "timestamps" :{
+                "start time": self.startTime,
+                "end time": self.endTIme
+            },
+            "scores": {
+                "postive": self.positive,
+                "neutral": self.neutral,
+                "negative": self.negative
+            }
+        }
---
+++ client/sentimentAnalysis.py
@@ -0,0 +1,41 @@
+
+import csv
+import urllib.request
+from transformers import AutoTokenizer, TFAutoModelForSequenceClassification, AutoModelForSequenceClassification
+from scipy.special import softmax
+import numpy as np
+from sentence import Sentence
+
+def analyzeSentiment(sent):
+
+        task='sentiment'
+        MODEL = f"cardiffnlp/twitter-roberta-base-sentiment"
+        tokenizer = AutoTokenizer.from_pretrained(MODEL)
+
+        #Labeling for sentiment (neg, pos, neu)
+        labels=[]
+        mapping_link = f"https://raw.githubusercontent.com/cardiffnlp/tweeteval/main/datasets/{task}/mapping.txt"
+        with urllib.request.urlopen(mapping_link) as f:
+            html = f.read().decode('utf-8').split("\n")
+            csvreader = csv.reader(html, delimiter='\t')
+        labels = [row[1] for row in csvreader if len(row) > 1]
+
+        model = AutoModelForSequenceClassification.from_pretrained(MODEL)
+        tokenizer.save_pretrained(MODEL)
+        model.save_pretrained(MODEL)
+        encoded_input = tokenizer(sent.sentence, return_tensors='pt')
+        output = model(**encoded_input)
+        scores = output[0][0].detach().numpy()
+        scores = softmax(scores) # transform's the model's output into a # between 0 -1 
+
+        sentimentScores = {}
+
+        for i in range(scores.shape[0]):
+
+            sentimentScores[labels[i]] = scores[i]
+
+        sent.negative = sentimentScores['negative']
+        sent.positive = sentimentScores['positive']
+        sent.neutral = sentimentScores['neutral']
+        
+        return sent
